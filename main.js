fd208327315d6c06ee4b22260b10ebd06aa1fc2d
Samson AKUMANI <samsakumani@gmail.com>
2022-10-25 12:06:14 +0000

    Utilities github page: Add Delay navigation

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const cards = document.querySelectorAll(".card")
+const links = document.querySelectorAll("a")
 
 cards.forEach(card => {
     card.addEventListener("click", () => {
         card.classList.toggle("selected")
+    })
+})
+links.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault()
+        setTimeout(() => { window.location = link.getAttribute("href") }, 5000);
     })
 })
