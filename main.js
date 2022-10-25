75c3fdc10a38465d51189ae2af2f0e65fb461ec3
Samson AKUMANI <samsakumani@gmail.com>
2022-10-25 12:42:01 +0000

    Utilities github page: Good now

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ cards.forEach(card => {
 })
 links.forEach(link => {
     link.addEventListener("click", async (event) => {
-        event.preventDefault()
         temp = 5
         link.parentNode.parentNode.append(`\nVous allez etre redirige dans ${
             temp
