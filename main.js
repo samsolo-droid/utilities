c22e81c52f1f3625a2511df40c5d01fd31bfda26
Samson AKUMANI <samsakumani@gmail.com>
2022-10-26 23:37:42 +0000

    Update 26/10/2022 23:37

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ links.forEach(link => {
     link.addEventListener("click", async (event) => {
         event.preventDefault()
         temp = 5
+        await setTimeout(() => { window.location = link.getAttribute("href") }, 5000);
         link.parentNode.parentNode.append(`\nVous allez etre redirige dans ${
             temp
         }
             seconde`)
-        await setTimeout(() => { window.location = link.getAttribute("href") }, 5000);
     })
 })
